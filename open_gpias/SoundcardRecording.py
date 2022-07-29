8a4d8ee955652796d2138a8e5d9b17e3be7d4f7d
Joaquin Prada <joaquiprada@gmail.com>
2022-07-28 21:45:58 -0300

    process_recording more numpynesco

--- open_gpias/SoundcardRecording.py
+++ open_gpias/SoundcardRecording.py
@@ -1,12 +1,7 @@
 import numpy as np
 
-def process_recording(rec_data, recording_rate, playback_data, playback_rate):
+def process_recording(rec_data, playback_data):
     num_data_points = len(rec_data)
-
-    pad_len = abs(len(rec_data) - len(playback_data))
-    playback_data = np.pad(playback_data, ((0,pad_len), (0,0)), 'constant', constant_values=(0))
-
-    # TODO: Optimize with numpy.recording_rate
     data = np.zeros((6, num_data_points), dtype=np.float64)
 
     # channel ai0: x-Data
@@ -15,16 +10,12 @@ def process_recording(rec_data, recording_rate, playback_data, playback_rate):
     # channel ai3: trigger pulse
     # channel ai4: pre-stimulus
     # channel ai5: startle-stimulus
-    
-    for rec_idx, rec_sample in enumerate(rec_data):
-        t = float(rec_idx) / recording_rate
-        pb_idx = int(t * playback_rate)
 
-        # TODO: Find better way to handle different samplerates
-        pb_sample = playback_data[pb_idx]
+    rec_data = np.stack(rec_data, axis=1)
+    playback_data = np.stack(playback_data, axis=1)
 
-        data[0][rec_idx] = data[1][rec_idx] = data[2][rec_idx] = float(rec_sample[0]) # xyz data
-        data[3][rec_idx] = data[5][rec_idx] = float(pb_sample[0]) # trigger pulse and startle-stimulus
-        data[4][rec_idx] = float(pb_sample[1]) # pre-stimulus
+    data[0] = data[1] = data[2] = rec_data[0]
+    data[3] = data[5] = playback_data[0]
+    data[4] = playback_data[1]
 
     return data
